1152d939ae2faaa93d50b819b9592e34a3abe87f
fahdroussafi <test.gmail@com>
2022-01-02 17:51:14 +0100

    commit

--- js/Reservation.js
+++ js/Reservation.js
@@ -1,60 +1,60 @@
-function remove_carburant() {
-    for (let i = 1; i < formulaire.carburant.length; i++) {
-        formulaire.carburant.options[i].innerText = '';
+function remove_fuel() {
+    for (let i = 1; i < form.fuel.length; i++) {
+        form.fuel.options[i].innerText = '';
     }
 }
-let boitedevitesse;
-formulaire.véhicule.addEventListener('change', function () {
-    let index = formulaire.véhicule.selectedIndex;
-    let carburant;
+let gearbox;
+form.vehicle.addEventListener('change', function () {
+    let index = form.vehicle.selectedIndex;
+    let fuel;
 
     // choose what is index of véhicule
     switch (index) {
         case 0:
-            remove_carburant();
+            remove_fuel();
             break;
         case 1:
-            remove_carburant();
-            carburant = ['Electrique', 'Essence'];
-            boitedevitesse = 'none';
+            remove_fuel();
+            fuel = ['Electrique', 'Essence'];
+            gearbox = 'none';
             break;
         case 2:
-            remove_carburant();
-            carburant = ['Hybirde', 'Essence', 'Diesel'];
-            boitedevitesse = 'Manuelle';
+            remove_fuel();
+            fuel = ['Hybirde', 'Essence', 'Diesel'];
+            gearbox = 'Manual';
             break;
         case 3:
-            remove_carburant();
-            carburant = ['Electrique', 'Hybirde', 'Essence', 'Diesel'];
-            boitedevitesse = 'Manuelle';
+            remove_fuel();
+            fuel = ['Electrique', 'Hybirde', 'Essence', 'Diesel'];
+            gearbox = 'Manual';
             break;
         case 4:
-            remove_carburant();
-            carburant = ['Diesel'];
-            boitedevitesse = 'Manuelle';
+            remove_fuel();
+            fuel = ['Diesel'];
+            gearbox = 'Manual';
             break;
         case 5:
-            remove_carburant();
-            carburant = ['Hybirde', 'Essence', 'Diesel'];
-            boitedevitesse = 'Automatique';
+            remove_fuel();
+            fuel = ['Hybirde', 'Essence', 'Diesel'];
+            gearbox = 'Automatique';
             break;
         case 6:
-            remove_carburant();
-            carburant = ['Diesel'];
-            boitedevitesse = 'Automatique';
+            remove_fuel();
+            fuel = ['Diesel'];
+            gearbox = 'Automatique';
             break;
         case 7:
-            remove_carburant();
-            carburant = ['Diesel', 'Essence'];
-            boitedevitesse = 'Manuelle';
+            remove_fuel();
+            fuel = ['Diesel', 'Essence'];
+            gearbox = 'Manual';
             break;
     }
 
-    // fill carburant et boite de vitesse
-    for (let i = 0; i < carburant.length; i++) {
-        formulaire.carburant.options[i + 1].innerText = carburant[i];
+    // fill fuel and gearbox
+    for (let i = 0; i < fuel.length; i++) {
+        form.fuel.options[i + 1].innerText = fuel[i];
     }
-    formulaire.boitedevitesse.options[1].innerText = boitedevitesse;
+    form.gearbox.options[1].innerText = gearbox;
 });
 
 document.querySelector("#select-submit").addEventListener('click', function (event) {
@@ -62,83 +62,83 @@ document.querySelector("#select-submit").addEventListener('click', function (eve
 
     // get value of select tags
     let cars = document.querySelector("#select > select:nth-child(3)").value;
-    let carcarburant = document.querySelector("#select > select:nth-child(4)").value;
+    let carfuel = document.querySelector("#select > select:nth-child(4)").value;
     let day = document.querySelector("#number").value;
     let price;
 
-    // calcule boite de vitesse none de moto
-    if (boitedevitesse == 'none') {
-        if (cars == 'moto') {
-            if (carcarburant == 'Electrique') {
+    // calcule boite de vitesse none de motorbike
+    if (gearbox == 'none') {
+        if (cars == 'motorbike') {
+            if (carfuel == 'Electrique') {
                 price = (10 * 0.05) + 10;
             }
-            if (carcarburant == 'Essence') {
+            if (carfuel == 'Essence') {
                 price = (10 * 0.14) + 10;
             }
         }
     }
     // calcule boite de vitesse auto
-    if (boitedevitesse == 'Automatique') {
+    if (gearbox == 'Automatique') {
         // calcule berlin
         if (cars == 'berlin') {
-            if (carcarburant == 'Hybirde') {
+            if (carfuel == 'Hybirde') {
                 price = ((20 * 0.19) + (20 * 0.09)) + 20;
             }
-            if (carcarburant == 'Essence') {
+            if (carfuel == 'Essence') {
                 price = ((20 * 0.19) + (20 * 0.14)) + 20;
             }
-            if (carcarburant == 'Diesel') {
+            if (carfuel == 'Diesel') {
                 price = ((20 * 0.19) + (20 * 0.21)) + 20;
             }
         }
-        // calcule camion
-        if (cars == 'camion') {
-            if (carcarburant == 'Diesel') {
+        // calcule truck
+        if (cars == 'truck') {
+            if (carfuel == 'Diesel') {
                 price = ((250 * 0.19) + (250 * 0.21)) + 250;
             }
         }
     }
-    // calcule boite de vitesse manuelle
-    if (boitedevitesse == 'Manuelle') {
+    // calcule boite de vitesse manual
+    if (gearbox == 'Manual') {
         // calcule compact
         if (cars == 'compact') {
-            if (carcarburant == 'Hybirde') {
+            if (carfuel == 'Hybirde') {
                 price = 14 + (14 * 0.09);
             }
-            if (carcarburant == 'Essence') {
+            if (carfuel == 'Essence') {
                 price = 14 + (14 * 0.14);
             }
-            if (carcarburant == 'Diesel') {
+            if (carfuel == 'Diesel') {
                 price = 14 + (14 * 0.21);
             }
         }
         // calcule citadin
         if (cars == 'citadin') {
-            if (carcarburant == 'Hybirde') {
+            if (carfuel == 'Hybirde') {
                 price = 12 + (12 * 0.09);
             }
-            if (carcarburant == 'Essence') {
+            if (carfuel == 'Essence') {
                 price = 12 + (12 * 0.14);
             }
-            if (carcarburant == 'Diesel') {
+            if (carfuel == 'Diesel') {
                 price = 12 + (12 * 0.21);
             }
-            if (carcarburant == 'Electrique') {
+            if (carfuel == 'Electrique') {
                 price = 12 + (12 * 0.05);
             }
         }
-        // calcule utilitaire
-        if (cars == 'utilitaire') {
-            if (carcarburant == 'Diesel') {
+        // calcule utility
+        if (cars == 'utility') {
+            if (carfuel == 'Diesel') {
                 price = 16 + (16 * 0.21);
             }
         }
-        // calcule engin de chantier
-        if (cars == 'engin de chantier') {
-            if (carcarburant == 'Diesel') {
+        // calcule construction machine
+        if (cars == 'construction machine') {
+            if (carfuel == 'Diesel') {
                 price = 900 + (900 * 0.21);
             }
-            if (carcarburant == 'Essence') {
+            if (carfuel == 'Essence') {
                 price = 900 + (900 * 0.14);
             }
         }
